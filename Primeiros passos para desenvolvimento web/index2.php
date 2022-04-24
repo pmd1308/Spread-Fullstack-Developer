6ffcea21d09a84927423e3c0242a685a342d35ab
pmd1308 <pmd1308@pm.me>
2022-04-24 20:36:17 -0300

    up

---
+++ Primeiros passos para desenvolvimento web/index2.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>Meu primeiro site em PHP! Woohoo!</title>
+    </head>
+    <script src='https://code.jquery.com/jquery-3.5.1.min.js'></script>
+    <style type='text/css'>
+        .linha {
+            font-weight: bold;
+            color: green;
+            padding-left: 10px;
+            line-height: 50px;
+        }
+    </style>
+    <body>
+        <?php
+            for ( $i = 0; $i < 10; i++) {
+                print('<span class=\"linha\"> número' . $i . '<\span><br />')
+            }
+        ?>
+    </body>
+    <script type='text/javascript'>
+        $(document).ready(function() {
+            alert('Woooo')
+        });
+    </script>
+</html>
